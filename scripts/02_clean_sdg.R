1457caaef6e3cc2dc8d9974912438e57e6d34f0c
Brayan Mora <brayanjp.mora@gmail.com>
2026-08-16 23:18:19 -0500

    feat: sdg cleaning setup script

---
+++ scripts/02_clean_sdg.R
@@ -0,0 +1,24 @@
+# ==============================================================================
+# Attitudes and prevalence of intimate partner violence
+# Author: Brayan Mora
+#
+# 02_clean_sdg.R
+#
+# Purpose: Read raw SDG goal5 data, filter by 5.2.1 sdg indicator, 
+#          standardize country codes to iso3n with countrycode package,
+#          aggregate to country level
+#
+# Input:   data/raw/sdg/ 
+# Output:  data/processed/ipv_country.rds
+# ==============================================================================
+
+# Setting up the environment ---------------------------------------------------
+install.packages("pacman")
+library("pacman")
+p_load(tidyverse, readxl, janitor, here, countrycode, haven, srvyr)
+## SDG Data --------------------------------------------------------------------
+sdg_goal5_path <- list.files(here("data", "raw", "sdg"), 
+                              pattern = "data-",
+                              full.names = TRUE)
+
+goal5_data <- read_xlsx(sdg_goal5_path, sheet = 2) 
