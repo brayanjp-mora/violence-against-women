5632077ddd97aec23940e8e00c57c703899c2ab6
Brayan Mora <brayanjp.mora@gmail.com>
2026-08-17 18:11:06 -0500

    refactor: reshaping dataset to select variables and isolate SDG indicator 5.2.1

--- scripts/02_clean_sdg.R
+++ scripts/02_clean_sdg.R
@@ -24,5 +24,6 @@ goal5_data <- read_xlsx(sdg_goal5_path, sheet = 2)
 
 ## Tidying Data ----------------------------------------------------------------
 goal5_data <- goal5_data |> 
-  clean_names()
-
+  clean_names() |> 
+  select(indicator, geo_area_code, value, age) |> 
+  filter(indicator == "5.2.1")
