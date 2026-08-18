f7b677252710543b598d4b4ce8f77caf490acc85
Brayan Mora <brayanjp.mora@gmail.com>
2026-08-17 21:17:41 -0500

    feat: add geo name, map geographic codes to iso3c & filter out regional aggregates

--- scripts/02_clean_sdg.R
+++ scripts/02_clean_sdg.R
@@ -25,13 +25,17 @@ goal5_data <- read_xlsx(sdg_goal5_path, sheet = 2)
 ## Tidying Data ----------------------------------------------------------------
 goal5_data <- goal5_data |> 
   clean_names() |> 
-  select(indicator, geo_area_code, value, age) |> 
+  select(indicator, geo_area_code, geo_area_name, value, age) |> 
   filter(indicator == "5.2.1")
 
 ## Cleaning Data ---------------------------------------------------------------
-goal5_data<- goal5_data |> 
+goal5_data <- goal5_data |> 
   # filtering by 15+ matches the open-ended adult age range 
   # of WVS sample (no upper cap) 
   filter(age == "15+")
 
-
+goal5_data <- goal5_data |> 
+  mutate(b_country_alpha = countrycode(
+                            geo_area_code, "un", "iso3c",
+                            custom_match = c("412" = "XKX"))) |> 
+  drop_na(b_country_alpha)
